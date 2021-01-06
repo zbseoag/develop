bd73ab3f277e92eec502fc46d541195acf4489ca
zbseoag <zbseoag@163.com>
2021-01-06 15:39:21 +0800

    日常更新

--- shell/demo.sh
+++ shell/demo.sh
@@ -7,6 +7,7 @@ demo(){
     #  do
     #      echo $line;
     #  done <&0;
+    
     local index=1
     for arg in "$@";do echo "参数 $index:   '$arg'";let index+=1; done
     echo ${@: -2}
@@ -15,60 +16,60 @@ demo(){
 }
 
 
+cat <<EOF
+$0             :脚本名称
+$#             :参数个数
+$*             :参数列表,空格分隔参数,无视引号
+"$*"           :所有参数看作一个字符串
+$@             :参数列表,空格分隔参数,无视引号
+"$@"           :可以区分引号
+$!             :进程 PID
+$_             :上一条命令最后一个参数值
+$$             :脚本自身的 PID
+$PPID          :父进程 PID
+$?             :上一条命令的退出状态码,0表成功,非0表失败
+$BASH          :Bash 程序路径
+$BASH_VERSION  :Bash 版本号
+$EUID          :有效用户 ID
+$UID           :用户 ID 号
+$GROUPS        :用户所属的组
+$HOSTNAME      :主机名称
+$IFS           :域分隔符,默认为空白符
+$OLDPWD        :上一个工作目录
+$PS1           :主提示符
+$PS2           :第二提示符,出现在需要补充输入时，默认值为 ">"
+$PS4           :第四提示符,使用 -x 选项调用脚本时，它会出现在每行开头，默认为"+"
+${#str}                       长度
+${str:3:5}                    按位置截取
+${str#*head} ${str##*head}    截断"
+${str%end*} ${str%%end*}      从后向前截断
+${str/find/replace}           替换一次
+${str//find/replace}          替换全部
+${str/#beginwith/replace}     前缀替换
+${str/%endwith/replace}       后缀替换
+${string -     'new'}     string 是否已存在？
+${string :-    'new'}     string 是否不为空？
+${string +     'new'}     string 是否不存在？
+${string :+    'new'}     string 是否为空？
+${string :+    'new'}     string 是否为空？
+${string =     'new'}     string 是否已存在？会改变 string 的值
+${string :=    'new'}     string 是否不为空？会改变 string 的值
+${string ?     'new'}     string 是否已存在？会报错
+${string :?    'new'}     string 是否不为空？会报错
+[[ "$str" =~ "this" ]]    是否包含
+${data[@]}                 所有元素
+${#data[@]}                长度
+${data[@]:start:end}       位置截取
+${data[@]#$1}  ${data[@]##$1} 查找
+${data[@]%$1}  ${data[@]%%$1} 从后面查找
+${data[@]/$1/$2}           替换
+${data[@]/#$1/$2} replace-head 
+${data[@]/%$1/$2} replace-end
 
-# 打印参数列表
-function args(){
-
-    local index=1
-    for arg in "$@"; do
-        echo "参数${index}: '$arg'"
-        let index+=1
-    done
+EOF
 
 
-    local all=("$@")
-    for((i=$#-1; i>=0; i--));  
-    do  
-        echo ${all[$i]}
-    done  
 
-}
-
-string(){
-
-    local str="11223344"
-    echo "${#str}                  长度"
-    echo "${str:3:5}               按位置截取"
-    echo "${str#*3} ${str##*3}     前截取后"
-    echo "${str%*3} ${str%%*3}     后截取前"
-    echo "${str/3/a}      替换一次"
-    echo "${str//3/a}     替换全部"
-    echo "${str/#11/a}    前缀替换"
-    echo "${str/%44/a}    后缀替换"
-
-    [[ "$str" =~ "this" ]] && echo "$str contains this" 
-}
-
-function array(){
-
-    local data=(${1//''/}) #字符串转数组
-    local option=$2
-
-    shift 2
-    case "$option" in
-        'len')              echo ${#data[@]};;
-        'cut')              if [ -z "$2" ];then echo ${data[@]:$1}; else echo ${data[@]:$1:$2}; fi;;
-        'find')             echo ${data[@]#$1};;
-        'find-long')        echo ${data[@]##$1};;
-        'find-end')         echo ${data[@]%$1};;
-        'find-end-long')    echo ${data[@]%%$1};;
-        'replace-head')     echo ${data[@]/#$1/$2};;
-        'replace-end')      echo ${data[@]/%$1/$2};;
-        'replace')          echo ${data[@]/$1/$2};;
-        *)                  echo ${data[@]};;
-    esac
-
-}
 
 function dock(){
 
@@ -101,31 +102,4 @@ function dock(){
 
 }
 
-function ddd(){
 
-cat <<EOF
-$0             :脚本名称
-$#             :参数个数
-$*             :参数列表,空格分隔参数,无视引号
-"$*"           :所有参数看作一个字符串
-$@             :参数列表,空格分隔参数,无视引号
-"$@"           :可以区分引号
-$!             :进程 PID
-$_             :上一条命令最后一个参数值
-$$             :脚本自身的 PID
-$PPID          :父进程 PID
-$?             :上一条命令的退出状态码,0表成功,非0表失败
-$BASH          :Bash 程序路径
-$BASH_VERSION  :Bash 版本号
-$EUID          :有效用户 ID
-$UID           :用户 ID 号
-$GROUPS        :用户所属的组
-$HOSTNAME      :主机名称
-$IFS           :域分隔符,默认为空白符
-$OLDPWD        :上一个工作目录
-$PS1           :主提示符
-$PS2           :第二提示符,出现在需要补充输入时，默认值为 ">"
-$PS4           :第四提示符,使用 -x 选项调用脚本时，它会出现在每行开头，默认为"+"
-EOF
-
-}
